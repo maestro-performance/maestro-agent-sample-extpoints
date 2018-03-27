65f2b6f3efd95554ac5932a544597aaf25a52711
Otavio Rodolfo Piske <opiske@redhat.com>
2018-03-27 20:09:00 +0200

    FIX: missing imports

--- requests/ping/PingHandler.groovy
+++ requests/ping/PingHandler.groovy
@@ -4,6 +4,9 @@ import org.maestro.agent.base.AbstractHandler
 import org.maestro.client.exchange.MaestroTopics
 import org.maestro.client.notes.OkResponse
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class PingHandler extends AbstractHandler {
     private static final Logger logger = LoggerFactory.getLogger(PingHandler.class);
 
