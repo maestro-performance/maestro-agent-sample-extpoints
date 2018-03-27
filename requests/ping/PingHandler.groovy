cdba45c6f16b9d37968d4e34e0f09075ac6e65bd
Otavio Rodolfo Piske <opiske@redhat.com>
2018-03-27 20:07:37 +0200

    UPD: to display the message on the log, instead of stdout

--- requests/ping/PingHandler.groovy
+++ requests/ping/PingHandler.groovy
@@ -5,10 +5,11 @@ import org.maestro.client.exchange.MaestroTopics
 import org.maestro.client.notes.OkResponse
 
 class PingHandler extends AbstractHandler {
+    private static final Logger logger = LoggerFactory.getLogger(PingHandler.class);
 
     @Override
     Object handle() {
-        println "Hello from a sample Maestro Extension point"
+        logger.info("Hello from a sample Maestro Extension point");
 
         this.getClient().publish(MaestroTopics.MAESTRO_TOPIC, new OkResponse())
         return null
